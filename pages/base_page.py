0e84efb66c1126a36e39883d9cec459a0b0a003e
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2025-12-10 02:01:05 +0200

    modified files

---
+++ pages/base_page.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10, poll_frequency=1)
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def is_opened(self, url):
+        self.wait.until(lambda d: url in d.current_url)
+
+    def find(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
+
+    def click(self, locator):
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
+
+    def type(self, locator, text):
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element.clear()
+        element.send_keys(text)
