dc7797080b8d394287b68122101388d872f6f034
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2025-12-15 01:12:16 +0200

    modified

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,33 +3,23 @@ from pages.base_page import BasePage
 
 class HomePage(BasePage):
     url = 'https://ramat-gan.muni.il'
+    toshavim_button = (By.XPATH, "//li[1]/ul/li[1]//span[2]")
+    # TO DO header_toshavim_page
+    toshavim_sherutim_hirum_button = (By.CSS_SELECTOR, "li.item-container:nth-child(10) > a:nth-child(1)")
+    header_asakim_button = (By.XPATH, "//li[1]/ul/li[2]//span[2]")
+    # TO DO header_asakim_page
+    asakim_asakimBeDigital_gmarHeshbon_button = (By.XPATH,"//app-header/app-main-menu-expanded/div[2]/div/div[2]/ul/li[4]/a")
 
-    header_toshavim_button = (
-        By.XPATH,
-        "//li[1]/ul/li[1]//span[2]"
-    )
-
-    toshavim_sherutim_hirum_button = (
-        By.CSS_SELECTOR,
-        "li.item-container:nth-child(10) > a:nth-child(1)"
-    )
-
-    header_asakim_button = (
-        By.XPATH,
-        "//li[1]/ul/li[2]//span[2]"
-    )
-
-    asakim_asakimBeDigital_gmarHeshbon_button = (
-        By.XPATH,
-        "//app-header/app-main-menu-expanded/div[2]/div/div[2]/ul/li[4]/a"
-    )
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.header_mivzakim_button = None
 
     def open_page(self):
         self.open(self.url)
         self.is_opened(self.url)
 
     def click_toshavim_button(self):
-        self.click(self.header_toshavim_button)
+        self.click(self.toshavim_button)
 
     def toshavim_sherutim_hirum_button_is_visible(self):
         return self.wait_visible(self.toshavim_sherutim_hirum_button)
@@ -39,3 +29,6 @@ class HomePage(BasePage):
 
     def asakim_asakimBeDigital_gmarHeshbon_button_is_visible(self):
         return self.wait_visible(self.asakim_asakimBeDigital_gmarHeshbon_button)
+
+    def click_mivzakim_button(self):
+        self.click(self.header_mivzakim_button)
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -8,8 +8,14 @@ class TestDemo(BaseTest):
         self.home_page.click_toshavim_button()
         assert self.home_page.toshavim_sherutim_hirum_button_is_visible()
 
-    # Header click on gסקים button and verify it's opened
+    # Header click on עסקים button and verify it's opened
     def test_asakim_opened(self):
         self.home_page.open_page()
         self.home_page.click_asakim_button()
         assert self.home_page.asakim_asakimBeDigital_gmarHeshbon_button_is_visible()
+
+    # Header click on מבזקים button and verify it's opened
+    # def test_mivzakim_opened(self):
+    #     self.home_page.open_page()
+    #     self.home_page.click_mivzakim_button()
+    #     assert self.home_page.title_is_visible()
