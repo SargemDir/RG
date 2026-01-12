ef4cd9378211af32b051b2c24447569d116e8a95
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2026-01-12 23:28:45 +0200

    109 page modified and test opened

--- tests/test_109.py
+++ tests/test_109.py
@@ -1,15 +1,15 @@
-# from time import sleep
-#
-# from tests.base_test import BaseTest
-#
-# class TestMoked(BaseTest):
-#
-#     # פנייה למוקד
-#     def test_109(self):
-#         self.interaction_form_page.open_page()
-#         self.interaction_form_page.check_aher_element_checkbox()
-#         self.interaction_form_page.click_on_emshech_lvl1_button()
-#         self.interaction_form_page.fill_data_and_go_to_next_page()
-#         self.interaction_form_page.upload_file_via_ui()
-#         sleep(10)
-#
+from time import sleep
+
+from tests.base_test import BaseTest
+
+class TestMoked(BaseTest):
+
+    # פנייה למוקד
+    def test_109(self):
+        self.interaction_form_page.open_page()
+        self.interaction_form_page.check_aher_element_checkbox()
+        self.interaction_form_page.click_on_emshech_lvl1_button()
+        self.interaction_form_page.fill_data_and_go_to_next_page()
+        self.interaction_form_page.upload_file_via_ui()
+        sleep(10)
+
