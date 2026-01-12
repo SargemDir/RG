18b40e5b2cfee0c2daec72e029b2ea8342975509
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2026-01-12 22:12:16 +0200

    conftest modified

--- conftest.py
+++ conftest.py
@@ -1,15 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import sys
 
 @pytest.fixture(scope="module")
 def driver():
     options = Options()
-    # options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
+    if sys.platform.startswith("linux"):
+        # For CI / Ubuntu
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("--window-size=2560,1440")
+        service = Service(ChromeDriverManager().install())
+    else:
+        # For Windows local
+        options.add_argument("--start-maximized")
+        service = Service(ChromeDriverManager().install())
+
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
